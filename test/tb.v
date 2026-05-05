4795358cf126ae785266c809cf8e0dbdfe7b3f06
pvc2k5 <23021224@vnu.edu.vn>
2026-05-05 07:02:37 +0700

    fix tb

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_pwm4ch dut (
+  tt_um_pwm_4ch dut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
